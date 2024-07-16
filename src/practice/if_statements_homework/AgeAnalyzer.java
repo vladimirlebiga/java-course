9cc697df3e9ccf43424e0e4673a16735a2146c4c
mariaprang <mprangishvili@newwork.com>
2024-07-16 23:22:29 +0400

    added homework instructions

---
+++ src/practice/if_statements_homework/AgeAnalyzer.java
@@ -0,0 +1,22 @@
+package practice.if_statements_homework;
+
+public class AgeAnalyzer {
+
+    public static void main(String[] args) {
+        // TODO: Declare hardcoded age variables for testing purposes.
+        // Example: int age1 = 12, age2 = 24, age3 = 45, age4 = 5, age5 = 65;
+
+        // TODO: For each hardcoded age variable, use if statements to determine the age category.
+
+        // TODO: For each age variable:
+        // 1. Check if the age is between 0 and 12 (inclusive). If true, print that the age is a "Child".
+        // 2. Check if the age is between 13 and 19 (inclusive). If true, print that the age is a "Teenager".
+        // 3. Check if the age is between 20 and 64 (inclusive). If true, print that the age is an "Adult".
+        // 4. Check if the age is 65 or older. If true, print that the age is a "Senior".
+
+        // TODO: Ensure to print a message for each age, indicating the age value and its corresponding category.
+        // Example message: "Age 12 is a Child"
+
+        // TODO: Repeat the above steps for all the hardcoded age variables to categorize and print the results.
+    }
+}
